88562d3331e225198db0c5fb2c7eb5aad0100ecd
Ion Filipski <armagedescu@gmail.com>
2025-07-22 17:59:20 +0300

    Add first version of shaderer

--- draw.simple.1.glsl300.vao.auto/2d.js
+++ draw.simple.1.glsl300.vao.auto/2d.js
@@ -3,7 +3,7 @@ let canvas = document.currentScript.parentElement;
 document.addEventListener('DOMContentLoaded', () =>
 {
    let context = canvas.getContext('2d');
-   context.font = '20pt Calibri';
+   context.font      = '20pt Calibri';
    context.fillStyle = 'green';
    context.fillText('Welcome to Tutorialspoint!', 10, 30);
 })
