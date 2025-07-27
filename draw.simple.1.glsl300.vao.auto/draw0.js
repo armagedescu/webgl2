d855360c43686c1caf10fad8d9b430d060ee565a
Ion Filipski <armagedescu@gmail.com>
2025-07-27 17:10:41 +0300

    Separate 1/2/3/4 dimensional vertices

--- draw.simple.1.glsl300.vao.auto/draw0.js
+++ draw.simple.1.glsl300.vao.auto/draw0.js
@@ -3,9 +3,10 @@ let canvas = document.currentScript.parentElement;
 
 let func = () =>
 {
-   let shape = new GlShape2Dv1 (canvas);
-   shape.vertices = [ 0.0, -0.5,   -0.5, 0.3,   -0.5, -0.6,
-                      0.0, -0.5,    0.8, 0.4,   -0.4,  0.5 ];
+   let shape = new GlShapev1 (canvas).withVertices2d(
+                     [ 0.0, -0.5,   -0.5, 0.3,   -0.5, -0.6,
+                       0.0, -0.5,    0.8, 0.4,   -0.4,  0.5 ]);
+
    let gl = shape.gl;
    gl.clearColor(0.5, 0.5, 0.5, 0.9);
    gl.enable(gl.DEPTH_TEST);
--- draw.simple.1.glsl300.vao.auto/translate.js
+++ draw.simple.1.glsl300.vao.auto/translate.js
@@ -1,40 +1,18 @@
 {
-
-class Translate extends GlVAObject
-{
-   #vertices =  [ 0.0, 0.0, 0.0,  -1.0, 0.4, 2.0,   -0.5, -0.6,  2.0,
-                  0.0, 0.0, 0.0,   0.4, 0.4, 2.0,   -0.4,  0.5, -0.0  ];
-
-   constructor(context)
-   {
-      super(context);
-      this.init();
-   }
-   init ()
-   {
-      this.bindVertexArray();
-      let gl = this.gl;
-
-      this.vertex_buffer = this.arrayBuffer(new Float32Array(this.#vertices));
-      this.coord = this.vertex_buffer.attrib ("coordinates",  3, gl.FLOAT);
-
-   }
-   drawVao()
-   {
-      let gl = this.gl;
-      gl.drawArrays(gl.TRIANGLES, 0, 6);
-   }
-}
+let canvas = document.currentScript.parentElement;
 
 let func = () =>
 {
-   let translate = new Translate('translate');
-   let gl = translate.gl;
-
-   translate.useProgram ();
+   let shape = new GlShapev1 (canvas)
+               .withStaticTranslation ([0.5, 0.5, 0.0, 0.0])
+               .withVertices3d ([ 0.0, 0.0, 0.0,  -1.0, 0.4, 2.0,   -0.5, -0.6,  2.0,
+                                  0.0, 0.0, 0.0,   0.4, 0.4, 2.0,   -0.4,  0.5, -0.0  ]);
+   let gl = shape.gl;
    gl.clearColor(0.5, 0.5, 0.5, 0.9);
-   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-   translate.draw ();
+   gl.enable (gl.DEPTH_TEST);
+   gl.clear  (gl.COLOR_BUFFER_BIT);
+   shape.drawTriangles();
 };
+
 document.addEventListener('DOMContentLoaded', func);
 }
