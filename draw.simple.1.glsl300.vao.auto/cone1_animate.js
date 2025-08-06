c7b5b8e7e89a0bf9627a6cb75707eaff48099429
Ion Filipski <armagedescu@gmail.com>
2025-08-06 22:41:36 +0300

    Add Uniform Light Direction

--- draw.simple.1.glsl300.vao.auto/cone1_animate.js
+++ draw.simple.1.glsl300.vao.auto/cone1_animate.js
@@ -1,65 +1,7 @@
 "use strict";
 {
-
 let canvas = document.currentScript.parentElement;
 
-class Cone1Animate1 extends GlVAObject
-{
-   #verts    = [];
-   #norms    = [];
-   #nh = null;
-   #ns = null;
-   #oldTime = 0;
-   #dt = 0;
-   constructor(context, nh = 1, ns = 5)
-   {
-      super(context);
-      this.initGeometry(nh, ns);
-      this.init();
-   }
-   initGeometry(nh, ns)
-   {
-      this.#nh = nh;
-      this.#ns = ns;
-      let  dr = 0.6;
-
-      for (let i = 0, ix = 0,iy = 1,iz = 2; i < this.#ns; i++, ix += 9,iy += 9,iz += 9)
-      {
-          [this.#verts[ix],     this.#verts[iy],     this.#verts[iz]]     = [0.0, 0.0, -0.7] ;//<-- tip of the cone, points to us
-          [this.#verts[ix + 3], this.#verts[iy + 3], this.#verts[iz + 3]] = [dr * Math.cos(2 * Math.PI * i / this.#ns),       dr * Math.sin(2 * Math.PI * i / this.#ns),      0] ;
-          [this.#verts[ix + 6], this.#verts[iy + 6], this.#verts[iz + 6]] = [dr * Math.cos(2 * Math.PI * (i+1) / this.#ns),   dr * Math.sin(2 * Math.PI * (i+1) / this.#ns),  0] ;
-
-          [this.#norms[ix],     this.#norms[iy],     this.#norms[iz]]     = [0, 0, 0] ;//<-- tip of the cone, points to us
-          [this.#norms[ix + 3], this.#norms[iy + 3], this.#norms[iz + 3]] = [this.#verts[ix + 3],   this.#verts[iy + 3],  -0.7] ;
-          [this.#norms[ix + 6], this.#norms[iy + 6], this.#norms[iz + 6]] = [this.#verts[ix + 6],   this.#verts[iy + 6],  -0.7] ;
-      }
-
-   }
-   init ()
-   {
-      this.bindVertexArray();
-      let gl = this.gl;
-
-      this.vertex_buffer = this.arrayBuffer(new Float32Array(this.#verts));
-      this.coord = this.vertex_buffer.attrib ("coordinates",  3, gl.FLOAT);
-
-      this.norms_buffer  = this.arrayBuffer(new Float32Array(this.#norms));
-      this.noord = this.norms_buffer.attrib  ("inputNormal", 3, gl.FLOAT);
-
-      this.lightDirection = gl.getUniformLocation(this.program, 'lightDirection');
-   }
-   set t(t) { this.#dt = t - this.#oldTime; }
-   drawVao()
-   {
-      let gl = this.gl;
-      //this.#dt = 0;
-      let lightx =  Math.cos (this.#dt * 0.002);
-      let lighty =  Math.sin (this.#dt * 0.002);
-      gl.uniform2f(this.lightDirection, lightx, lighty);
-      //gl.uniform2fv(this.lightDirection, [Math.cos (0 * 0.002),  Math.sin (0 * 0.002)]);
-      gl.drawArrays(gl.TRIANGLES, 0, this.#ns * 3);
-   }
-}
 function getCone (nhe, nse, type = Float32Array)
 {
    let nh = nhe;
@@ -82,67 +24,65 @@ function getCone (nhe, nse, type = Float32Array)
    return {verts:new type(verts), norms:new type(norms)};
 }
 
-let vs =
-`
+let vs = `
 #version 300 es
-layout (location = 0) in vec3 vertices;
-layout (location = 1) in vec3 normals;
-out vec3 normalsVary;
+layout (location = 0) in vec3 vertex;
+layout (location = 1) in vec3 normal;
+out vec3 normalVary;
 void main(void)
 {
-   gl_Position = vec4(vertices, 1);
-   normalsVary=normals;
+   gl_Position = vec4(vertex, 1);
+   normalVary = normal;
 }`;
 
 
-let fs = 
-`
+let fs = `
 #version 300 es
 precision mediump float;
 out vec4 fragColor;
 const vec4 constColor = vec4 (0.0, 1.0, 0.0, 1.0);
-in  vec3 normalsVary;
-const vec3 constLightDirection = vec3(1.0, 0.0, 1.0);
+in  vec3 normalVary;
+uniform vec3 lightDirection;
 void main(void)
 {
-   float prod = -dot (normalize(constLightDirection),  normalize(normalsVary) );
+   float prod = -dot (normalize(lightDirection),  normalize(normalVary) );
    fragColor = vec4 (constColor.rgb * prod,  1.0);
 }`;
 
 let func = () =>
 {
-   let gl;
+   let gl, animate;
    let geo = getCone(1, 5);
    let shape = new GlShapev1 (canvas)
+      .withShaderSources (vs, fs)
       .withConstColor ([0.0, 1.0, 0.0, 1.0])
       .withVertices3d (geo.verts)
       .withNormals3d  (geo.norms)
-      .withConstLightDireciton ([Math.cos (0 * 0.002),  Math.sin (0 * 0.002), 1])
-      .withShaderSources (vs, fs);
+      .withLightDirection3f ([1.0, 0.0, 1.0])
+      ;
+   //shape.logStrategyShaders ("cone1_animate.js");
    gl = shape.gl;
    gl.clearColor(0.5, 0.5, 0.5, 0.9);
-   gl.enable(gl.DEPTH_TEST);
-   gl.clear (gl.COLOR_BUFFER_BIT);
-   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-   shape.logStrategyShaders ("cone1_animate.js");
+   gl.enable (gl.DEPTH_TEST);
+   gl.clear  (gl.COLOR_BUFFER_BIT);
+   gl.clear  (gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+   shape.bind ();
+   shape.lightDirection3 ([1.0, 0.0, 1.0]);
    shape.drawTriangles ();
-   return;
 
-   let cone1Animate1 = new Cone1Animate1(canvas);  
-   gl = cone1Animate1.gl;
-   let animate = (time) =>
+   animate = (time) =>
    {
-      cone1Animate1.useProgram ();
       gl.clearColor(0.5, 0.5, 0.5, 0.9);
       gl.enable(gl.DEPTH_TEST);
       gl.clear (gl.COLOR_BUFFER_BIT);
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      cone1Animate1.t = time;
-      cone1Animate1.draw();
-
+      shape.lightDirection3 ([Math.cos (time * 0.002),  Math.sin (time * 0.002), 1]);
+      shape.drawTriangles ();
       window.requestAnimationFrame(animate);
    }
    animate(0);
+   return;
+
 
 };
 document.addEventListener('DOMContentLoaded', func);
