4a9e27393396dd1f314e11f5fb140d85e94fe166
Ion Filipski <armagedescu@gmail.com>
2025-10-06 01:02:33 +0300

    Refactor rename remove webgpu prefix

--- webgpu1/webgpuSimpleDrawIndexed.js
+++ webgpu1/webgpuSimpleDrawIndexed.js
@@ -4,8 +4,6 @@ let canvas = document.currentScript.parentElement;
 
 let clearColor = [0.5, 0.5, 0.5, 0.9];
 let vertices  = new Float32Array ([ 0.5, -0.5,    1.0, 1.0,   -1.0,  1.0,  -1.0, -1.0]);
-////to be changed to
-//let vertices  = [ 1.0, -1.0,    1.0, 1.0,   -1.0,  1.0,  -1.0, -1.0];
 let indices   = new Uint16Array ([0, 1, 2, 0, 2, 3]);
 
 let func = async () =>
@@ -87,34 +85,6 @@ let func = async () =>
    passEncoder.end();
 
    device.queue.submit([commandEncoder.finish()]);
-
-   //let glCanvas = new GlCanvas('simpleDrawIndexed');
-   //let gl = glCanvas.gl;
-   //glCanvas.useProgram ();
-//
-   //gl.clearColor(0.5, 0.5, 0.5, 0.9);
-   //gl.enable(gl.DEPTH_TEST);
-   //gl.clear (gl.COLOR_BUFFER_BIT);
-//
-   //let vertices  = [ 0.5, -0.5,    1.0, 1.0,   -1.0,  1.0,  -1.0, -1.0];
-   //////to be changed to
-   ////let vertices  = [ 1.0, -1.0,    1.0, 1.0,   -1.0,  1.0,  -1.0, -1.0];
-   //let indices   = [0, 1, 2, 0, 2, 3];
-//
-   //// Create a new buffer object
-   //let vertex_buffer = gl.createBuffer();
-   //gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-   //gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-//
-   //let coord = gl.getAttribLocation (glCanvas.program, "coordinates");
-   //gl.vertexAttribPointer     (coord, 2, gl.FLOAT, false, 0, 0); //point an attribute to the currently bound VBO
-   //gl.enableVertexAttribArray (coord); //Enable the attribute
-//
-   //let idxBuffer = gl.createBuffer();
-   //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, idxBuffer);
-   //gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(indices), gl.STATIC_DRAW);
-//
-   //gl.drawElements (gl.TRIANGLES, indices.length, gl.UNSIGNED_INT, 0);
 
 };
 document.addEventListener('DOMContentLoaded', func);
